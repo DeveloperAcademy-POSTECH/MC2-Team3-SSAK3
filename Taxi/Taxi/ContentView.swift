a4ec7d8a27554246b0bd25962d51437f1320e8f4
HoJong <pjh00098@gmail.com>
2022-05-27 19:42:12 +0900

    Chore: Add Xcode project
    ios 템플릿 어플리케이션 생성

---
+++ Taxi/Taxi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Taxi
+//
+//  Created by JongHo Park on 2022/05/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Taxi/Taxi/TaxiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxiApp.swift
+//  Taxi
+//
+//  Created by JongHo Park on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct TaxiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
