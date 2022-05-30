79f6f29bc17478d19cae2241cbecb93adc7e6ad5
HoJong <pjh00098@gmail.com>
2022-05-30 16:37:47 +0900

    Chore: Add firebase dependency
    파이어베이스 프레임워크 추가

--- Taxi/Taxi/TaxiApp.swift
+++ Taxi/Taxi/TaxiApp.swift
@@ -5,10 +5,14 @@
 //  Created by JongHo Park on 2022/05/27.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct TaxiApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
