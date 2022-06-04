adfdad622cbeea477cb4a5c7e3d8034ceb6f1e02
HoJong <pjh00098@gmail.com>
2022-06-04 14:33:40 +0900

    Feat: Add Message Model

--- Taxi/Taxi/Model/Message.swift
+++ Taxi/Taxi/Model/Message.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+struct Message {
+    let sender: String // (보낸 유저의 고유 id)
+    let body: String // 채팅 메시지
+    let timeStamp: Int // (yyyyMMddhhmm)
+    let typeCode: Int // (0 - 일반 채팅, 1 - 입장 채팅)
+}
+
+extension Message: CustomStringConvertible {
+    var description: String {
+        switch type {
+        case .normal:
+            return "메시지 내용: \(body) 보낸 시간: \(timeStamp)"
+        case .entrance:
+            return "입장 유저: \(sender) 입장 시간: \(timeStamp)"
+        }
+    }
+}
+
+extension Message {
+    var type: MessageType {
+        switch typeCode {
+        case 0:
+            return .normal
+        case 1:
+            return .entrance
+        default:
+            fatalError("알 수 없는 메시지 타입")
+        }
+    }
+}
+
+extension Message {
+    enum MessageType {
+        case normal
+        case entrance
+    }
+}
