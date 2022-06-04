2d4ab277bd42cfd327a241b73318cbc6821b65f4
HoJong <pjh00098@gmail.com>
2022-06-04 15:06:51 +0900

    Feat: Add TaxiParty

---
+++ Taxi/Taxi/Model/TaxiParty.swift
@@ -0,0 +1,35 @@
+//
+//  TaxiParty.swift
+//  Taxi
+//
+//  Created by JongHo Park on 2022/06/04.
+//
+
+import Foundation
+
+struct TaxiParty: Codable {
+    let id: String
+    let departureCode: Int
+    let destinationCode: Int
+    let meetingDate: Int // (YYYYMMdd)
+    let meetingTime: Int // (hhmm)
+    let maxPersonNumber: Int
+    let memebers: [String]
+    let isClosed: Bool
+}
+
+extension TaxiParty: CustomStringConvertible {
+    var description: String {
+        "출발 날짜: \(meetingDate) 출발 시간: \(meetingTime) 출발지: \(Place.of(departureCode).rawValue) 도착지: \(Place.of(destinationCode).rawValue)"
+    }
+}
+
+extension TaxiParty {
+    var departure: String {
+        Place.of(departureCode).rawValue
+    }
+
+    var destincation: String {
+        Place.of(destinationCode).rawValue
+    }
+}
