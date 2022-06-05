a5c70a33b7b037e85e0f9f2d7b46002fa0b2417f
HoJong <pjh00098@gmail.com>
2022-06-05 21:12:29 +0900

    Feat: Add nickname change
    닉네임 변경 함수 구현

--- Taxi/Taxi/Domain/Model/User.swift
+++ Taxi/Taxi/Domain/Model/User.swift
@@ -9,12 +9,12 @@ import Foundation
 
 struct User: Codable {
     let id: String // 기기 고유의 uuid 를 사용해 유저를 식별한다.
-    let nickName: String // 유저 닉네임
+    let nickname: String // 유저 닉네임
     let profileImage: String? // 프로필 이미지
 }
 
 extension User: CustomStringConvertible {
     var description: String {
-        "유저 닉네임: \(nickName) 프로필 이미지 url: \(profileImage ?? "없음")"
+        "유저 닉네임: \(nickname) 프로필 이미지 url: \(profileImage ?? "없음")"
     }
 }
