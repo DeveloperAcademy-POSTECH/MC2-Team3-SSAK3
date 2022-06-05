7e302e01944aff4e6e40a66384bd9c2c96e0abe3
HoJong <pjh00098@gmail.com>
2022-06-05 17:29:47 +0900

    Feat: Add User Repository Protocol
    User를 관리하는 추상화 계층인 Repository Protocol 생성

---
+++ Taxi/Taxi/Domain/Interface/UserRepository.swift
@@ -0,0 +1,16 @@
+//
+//  UserRepository.swift
+//  Taxi
+//
+//  Created by JongHo Park on 2022/06/05.
+//
+
+import Combine
+import Foundation
+
+protocol UserRepository {
+    func setUser(_ id: String, _ nickname: String) -> AnyPublisher<User, Error>
+    func getUser(_ id: User) -> AnyPublisher<User, Error>
+    func updateProfileImage(_ user: User, _ imageUrl: String) -> AnyPublisher<User, Error>
+    func updateNickname(_ user: User, _ nickname: String) -> AnyPublisher<User, Error>
+}
