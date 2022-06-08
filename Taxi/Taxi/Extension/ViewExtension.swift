e51f48366d0a2a3cbb0bbaf0a8cec94d875c0e02
SH0123 <sanghyoboy@naver.com>
2022-06-08 20:44:34 +0900

    Feat: "가입 코드 체크 로직 구현"
    가입 코드 체크 로직 구현 체크 로직에 따른 텍스트 문구 변경 Related to : #17

--- Taxi/Taxi/Extension/ViewExtension.swift
+++ Taxi/Taxi/Extension/ViewExtension.swift
@@ -9,8 +9,14 @@ import Foundation
 import SwiftUI
 
 extension View {
+    // signUp 페이지 textfield underline 추가
     func underlineTextField() -> some View {
         self
-            .overlay(Rectangle().frame(height: 2).foregroundColor(.gray.opacity(0.3)).padding(.top, 40))
+            .overlay(Rectangle().frame(height: 2).foregroundColor(.gray.opacity(0.3)).padding(.top, 40).padding(.bottom, 20))
+    }
+
+    // 가입 code check후 변화하는 text modifier
+    func customTextStyle(isTrue: Bool) -> some View {
+        modifier(CustomTextModifier(isTrue: isTrue))
     }
 }
--- Taxi/Taxi/Presenter/SignUpView.swift
+++ Taxi/Taxi/Presenter/SignUpView.swift
@@ -10,26 +10,54 @@ import SwiftUI
 struct SignUpView: View {
     @State private var signUpCode: String = ""
     @State private var nickName: String = ""
+    @State private var codeIsTrue: Bool = false
+    private let developerCode: String = "가즈윌"
 
     var body: some View {
         VStack(alignment: .leading) {
             Text("가입 코드").fontWeight(.bold).opacity(0.3)
-            makeTextField(self.$signUpCode)
+            VStack(alignment: .trailing) {
+                makeTextField(self.$signUpCode) {typedCode in
+                    codeIsTrue = (typedCode == developerCode)
+                }
+                Text(codeIsTrue ? "✅ 인증 완료되었습니다" : "* 최초 인증 및 가입에 활용됩니다.").customTextStyle(isTrue: codeIsTrue)
+            }
             Text("닉네임").fontWeight(.bold).opacity(0.3)
-            makeTextField(self.$nickName)
+            VStack(alignment: .trailing) {
+                makeTextField(self.$nickName)
+                Group {
+                    Text("* 사용하실 닉네임을 입력해주세요").opacity(0.3)
+                    Text("(아카데미 내의 닉네임을 권장드립니다)").opacity(0.3)
+                }.font(.caption)
+            }
             Spacer()
             Button(action: {print("button clicked")}){
                 Text("버튼")
-            }
-            .frame(maxWidth: .infinity)
+                    .frame(maxWidth: .infinity)
+                }.disabled(!codeIsTrue || nickName.isEmpty)
         }
         .padding()
     }
-    private func makeTextField( _ inputString: Binding<String>) -> some View {
+
+    private func makeTextField( _ inputString: Binding<String>, _ codeCheckHandler: ((String) -> Void)? = nil) -> some View {
         TextField("", text: inputString)
             .textInputAutocapitalization(.never)
             .disableAutocorrection(true)
+            .onSubmit {
+                codeCheckHandler?(inputString.wrappedValue)
+            }
             .underlineTextField()
+    }
+}
+
+struct CustomTextModifier: ViewModifier {
+    var isTrue: Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.caption)
+            .foregroundColor(isTrue ? .green : .black)
+            .opacity(isTrue ? 1 : 0.3)
     }
 }
 
