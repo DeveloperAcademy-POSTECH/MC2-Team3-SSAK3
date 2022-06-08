7adb7c2003ffea29c58d9b6821bd70d70655bd65
SH0123 <sanghyoboy@naver.com>
2022-06-08 22:32:10 +0900

    Fix: "코드 리뷰를 바탕으로 코드 개선"
    변수명, 함수명에 대한 변경
    필요없는 custom modifier 제거
    일관성있는 코드 스타일 유지
    related to #17

--- Taxi/Taxi/Extension/ViewExtension.swift
+++ Taxi/Taxi/Extension/ViewExtension.swift
@@ -14,9 +14,4 @@ extension View {
         self
             .overlay(Rectangle().frame(height: 2).foregroundColor(.gray.opacity(0.3)).padding(.top, 40).padding(.bottom, 20))
     }
-
-    // 가입 code check후 변화하는 text modifier
-    func customTextStyle(isTrue: Bool) -> some View {
-        modifier(CustomTextModifier(isTrue: isTrue))
-    }
 }
