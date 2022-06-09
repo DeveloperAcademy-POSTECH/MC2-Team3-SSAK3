7cc6fc58df5610cf8303251fda12c1478bf2c86b
Yosep <dlstj1212@naver.com>
2022-06-09 15:16:01 +0900

    Feat: Add TaxiPartyList's SwiftLint message fix

--- Taxi/Taxi/Presenter/TaxiPartyListView.swift
+++ Taxi/Taxi/Presenter/TaxiPartyListView.swift
@@ -9,63 +9,61 @@ import SwiftUI
 
 struct TaxiPartyListView: View {
     var body: some View {
-        VStack{
-            HStack{
-            Text("택시팟")
-                .font(.title)
-                .fontWeight(.bold)
+        VStack {
+            HStack {
+                Text("택시팟")
+                    .font(.title)
+                    .fontWeight(.bold)
                 Spacer()
-                Button(action: {
+                Button {
                     print("+ tapped!")
-                })
-                {
-            Image(systemName: "plus")
-                .resizable()
-                .frame(width: 25, height: 25)
-                .foregroundColor(.black)
+                } label: {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .foregroundColor(.black)
                 }
             }
             Spacer(minLength: 20)
-            HStack{
-                Button(action: {
+            HStack {
+                Button {
                     print("전체 tapped!")
-                })
-                {
-            Text("전체")
-                    .padding(5)
-                }.foregroundColor(.black)
-                Button(action: {
+                } label: {
+                    Text("전체")
+                        .padding(5)
+                        .foregroundColor(.black)
+                }
+                Button {
                     print("포항역 tapped!")
-                })
-                {
-            Text("포항역")
-                    .padding(5)
-                }.foregroundColor(.black)
-                Button(action: {
+                } label: {
+                    Text("포항역")
+                        .padding(5)
+                        .foregroundColor(.black)
+                }
+                Button {
                     print("포스텍 tapped!")
-                })
-                {
-            Text("포스텍")
-                    .padding(5)
-                }.foregroundColor(.black)
-            Spacer()
-                Button(action: {
-                    print("날짜 변경 tapped!")
-                })
-                {
-            Text("날짜 변경")
-              .foregroundColor(.yellow)
-              .fontWeight(.bold)
+                } label: {
+                    Text("포스텍")
+                        .padding(5)
+                        .foregroundColor(.black)
+                }
+                Spacer()
+                Button {
+                    print("날짜변경 tapped!")
+                } label: {
+                    Text("날짜 변경")
+                        .foregroundColor(.yellow)
+                        .fontWeight(.bold)
                 }
             }
             Spacer(minLength: 20)
-            ScrollView{
+            ScrollView {
                 ForEach(0..<50) { num in
                     Text("Test: \(num)")
                 }
             }
         }.padding(20)
-}
+    }
 }
 
 struct TaxiPartyListView_Previews: PreviewProvider {
