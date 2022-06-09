334df08fc9813343e14de21060d63c12304ef3ed
HoJong <pjh00098@gmail.com>
2022-06-09 23:46:31 +0900

    Feat: Add Tab View & Navigation View

---
+++ Taxi/Taxi/Constant/ImageName.swift
@@ -0,0 +1,17 @@
+//
+//  ImageName.swift
+//  Taxi
+//
+//  Created by JongHo Park on 2022/06/09.
+//
+
+import Foundation
+
+enum ImageName {
+    static let tabTaxiPartyOff: String = "TaxiPartyOff"
+    static let tabMyPartyOff: String = "MyPartyOff"
+    static let tabMyPageOff: String = "MyPageOff"
+    static let tabTaxiPartyOn: String = "TaxiPartyOn"
+    static let tabMyPartyOn: String = "MyPartyOn"
+    static let tabMyPageOn: String = "MyPageOn"
+}
---
+++ Taxi/Taxi/Presenter/RootView.swift
@@ -0,0 +1,57 @@
+//
+//  RootView.swift
+//  Taxi
+//
+//  Created by JongHo Park on 2022/06/09.
+//
+
+import SwiftUI
+
+enum Tab {
+    case taxiParty
+    case myParty
+    case setting
+}
+struct RootView: View {
+    @State private var currentTab: Tab = .taxiParty
+    var body: some View {
+        NavigationView {
+            TabView(selection: $currentTab) {
+                Text("택시 파티")
+                    .tabItem {
+                        if currentTab == .taxiParty {
+                            Label("택시팟", image: ImageName.tabTaxiPartyOn)
+                        } else {
+                            Label("택시팟", image: ImageName.tabTaxiPartyOff)
+                        }
+                    }
+                    .tag(Tab.taxiParty)
+                Text("마이 파티")
+                    .tabItem {
+                        if currentTab == .myParty {
+                            Label("마이팟", image: ImageName.tabMyPartyOn)
+                        } else {
+                            Label("마이팟", image: ImageName.tabMyPartyOff)
+                        }
+                    }
+                    .tag(Tab.myParty)
+                Text("설정")
+                    .tabItem {
+                        if currentTab == .setting {
+                            Label("설정", image: ImageName.tabMyPageOn)
+                        } else {
+                            Label("설정", image: ImageName.tabMyPageOff)
+                        }
+                    }
+                    .tag(Tab.setting)
+            }
+            .accentColor(.brand)
+        }
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
