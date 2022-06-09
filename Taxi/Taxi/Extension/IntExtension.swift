c37830013ac73e04b735f9ce2a06e113c534327f
SH0123 <sanghyoboy@naver.com>
2022-06-10 08:37:07 +0900

    Feat: "custom 달력 today, selectDay"
    custom 달력 월별 이동 가능
    today 표시
    select day 선택하여 표시 가능
    월 별 이동할 때 frame 문제 해결 해야함
    related to #22

---
+++ Taxi/Taxi/Extension/IntExtension.swift
@@ -0,0 +1,26 @@
+//
+//  IntExtension.swift
+//  Taxi
+//
+//  Created by sanghyo on 2022/06/10.
+//
+
+import Foundation
+
+extension Int {
+    /// yyyyMMdd의 Int 형태 데이터를 Date 타입으로 변경
+    func intToDate() -> Date {
+        let dateStr = String(self)
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+
+        return formatter.date(from: dateStr) ?? Date()
+    }
+
+    /// yyyyMMdd의 Int 형태 데이터를 MM월 dd
+    func intToString() -> String {
+        let date = self.intToDate()
+
+        return "\(date.month)월 \(date.day)일"
+    }
+}
