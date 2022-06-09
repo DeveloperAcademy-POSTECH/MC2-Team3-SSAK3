27dee3760371be92ae3cc38d66691a375c301f05
Yosep <dlstj1212@naver.com>
2022-06-09 14:44:16 +0900

    Feat: Add TaxiPartyList’s Button Click Log

--- Taxi/Taxi/Presenter/TaxiPartyListView.swift
+++ Taxi/Taxi/Presenter/TaxiPartyListView.swift
@@ -15,24 +15,50 @@ struct TaxiPartyListView: View {
                 .font(.title)
                 .fontWeight(.bold)
                 Spacer()
+                Button(action: {
+                    print("+ tapped!")
+                })
+                {
             Image(systemName: "plus")
                 .resizable()
                 .frame(width: 25, height: 25)
                 .foregroundColor(.black)
+                }
             }
             Spacer(minLength: 20)
             HStack{
+                Button(action: {
+                    print("전체 tapped!")
+                })
+                {
             Text("전체")
-                    .padding(2)
+                    .padding(5)
+                }.foregroundColor(.black)
+                Button(action: {
+                    print("포항역 tapped!")
+                })
+                {
             Text("포항역")
-                    .padding(2)
+                    .padding(5)
+                }.foregroundColor(.black)
+                Button(action: {
+                    print("포스텍 tapped!")
+                })
+                {
             Text("포스텍")
-                    .padding(2)
+                    .padding(5)
+                }.foregroundColor(.black)
             Spacer()
-            Text("날짜변경")
+                Button(action: {
+                    print("날짜 변경 tapped!")
+                })
+                {
+            Text("날짜 변경")
               .foregroundColor(.yellow)
               .fontWeight(.bold)
+                }
             }
+            Spacer(minLength: 20)
             ScrollView{
                 ForEach(0..<50) { num in
                     Text("Test: \(num)")
