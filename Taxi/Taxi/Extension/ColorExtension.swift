b3800dd9301ed12d298451d7c9641e634e15b5e7
Seungyeon <jsyworks@gmail.com>
2022-06-10 21:05:47 +0900

    Feat: Add Color & textStyle

--- Taxi/Taxi/Extension/ColorExtension.swift
+++ Taxi/Taxi/Extension/ColorExtension.swift
@@ -3,10 +3,19 @@
 //  Taxi
 //
 //  Created by JongHo Park on 2022/06/09.
-//
 
 import SwiftUI
 
 extension Color {
-    static let brand = Color("Brand")
+    static let customYellow = Color("customYellow")
+    static let customBlack = Color("customBlack")
+    static let charcoal = Color("charcoal")
+    static let darkGray = Color("darkGray")
+    static let customGray = Color("customGray")
+    static let lightGray = Color("lightGray")
+    static let background = Color("background")
+    static let customRed = Color("customRed")
+    static let postechPink = Color("postechPick")
+    static let ktxBlue = Color("ktxBlue")
 }
+
---
+++ Taxi/Taxi/Extension/TextStyleExtension.swift
@@ -0,0 +1,81 @@
+//
+//  TextStyleExtension.swift
+//  Taxi
+//
+//  Created by joy on 2022/06/10.
+//
+
+
+//google search keyword : swiftUI text style, swiftUI text custome, swiftUI text Modifier
+
+import Foundation
+import SwiftUI
+
+struct MainTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        return content
+            .foregroundColor(.customBlack)
+            .font(Font.custom("AppleSDGothicNeo-Bold", size: 26))
+    }
+}
+
+struct SubTitle: ViewModifier{
+    func body(content: Content) -> some View {
+        return content
+            .foregroundColor(.customGray)
+            .font(Font.custom("AppleSDGothicNeo-Medium", size: 18))
+    }
+}
+
+struct SubTitleSelect: ViewModifier{
+    func body(content: Content) -> some View {
+        return content
+            .foregroundColor(.darkGray)
+            .font(Font.custom("AppleSDGothicNeo-Medium", size: 18))
+    }
+}
+
+struct ChatStyle: ViewModifier{
+    func body(content: Content) -> some View {
+        return content
+            .foregroundColor(.customBlack)
+            .font(Font.custom("AppleSDGothicNeo-Medium", size: 16))
+    }
+}
+
+struct InchatNotification: ViewModifier{
+    func body(content: Content) -> some View {
+        return content
+            .foregroundColor(.customBlack)
+            .font(Font.custom("AppleSDGothicNeo-Regular", size: 10))
+    }
+}
+
+struct ListMainTitle: ViewModifier{
+    func body(content: Content) -> some View {
+        return content
+            .foregroundColor(.customBlack)
+            .font(Font.custom("AppleSDGothicNeo-Regular", size: 10))
+    }
+}
+
+extension View {
+    func mainTitle() -> some View {
+        self.modifier(MainTitle())
+    }
+    func subTitle() -> some View {
+        self.modifier(SubTitle())
+    }
+    func subTitleSelect() -> some View {
+        self.modifier(SubTitleSelect())
+    }
+    func chatStyle() -> some View {
+        self.modifier(ChatStyle())
+    }
+    func inchatNotification() -> some View {
+        self.modifier(InchatNotification())
+    }
+    func listMaintitle() -> some View {
+        self.modifier(ListMainTitle())
+    }
+}
