8dc5e076e5371ad0873d7e2faab598be16c19e45
Yosep <dlstj1212@naver.com>
2022-06-11 03:41:47 +0900

    Feat: Add TaxiParty DumyData CellView in ScrollView
    추가로 refresh UI

---
+++ Taxi/Taxi/Presenter/CellView.swift
@@ -0,0 +1,123 @@
+//
+//  CellView.swift
+//  Taxi
+//
+//  Created by Yosep on 2022/06/09.
+//
+import SwiftUI
+
+struct CellView: View {
+    var body: some View {
+           // Color(red: 248 / 255, green: 248 / 255, blue: 248 / 255, opacity: 1.0)
+            VStack(alignment: .leading, spacing: 13) {
+                HStack {
+                    Spacer().frame(width: 6)
+                    MeetingTimeCell()
+                    Spacer().frame(width: 130)
+                    CurrentUserCell()
+                        .padding(EdgeInsets(top: 0, leading: 15, bottom: 10, trailing: 1))
+                }
+                DestinationCell()
+            }
+            .frame(width: 339, height: 119)
+            .background(RoundedRectangle(cornerRadius: 18.0).fill(Color(.white)))
+            .overlay(
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(Color.gray, lineWidth: 1.5)
+                    .opacity(0.17)
+                    .shadow(color: .gray, radius: 1, x: 0, y: 0)
+            )
+            .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+    }
+} // 전체적인 font를 "Apple SF 산돌고딕 Neo"로 교체 예정
+
+struct MeetingTimeCell: View {
+    var body: some View {
+        Text("13:30")
+            .font(.custom("Apple SD Gothic Neo", size: 40))
+            .fontWeight(.bold) // 데이터로 처리 필요
+            .foregroundColor(Color(red: 64 / 255, green: 64 / 255, blue: 64 / 255, opacity: 1.0))
+    }
+}
+
+struct CurrentUserCell: View {
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 4) {
+            Image(systemName: "person.fill")
+                .resizable()
+                .frame(width: 16, height: 16)
+                .foregroundColor(Color(red: 64 / 255, green: 64 / 255, blue: 64 / 255, opacity: 1.0))
+                .padding(EdgeInsets(top: 1, leading: 1, bottom: 4, trailing: 1))
+            Text("3") // 데이터로 처리 필요
+                .foregroundColor(Color(red: 64 / 255, green: 64 / 255, blue: 64 / 255, opacity: 1.0))
+                .font(.custom("Apple SD Gothic Neo", size: 20))
+            Text("/")
+                .foregroundColor(Color(red: 206 / 255, green: 206 / 255, blue: 206 / 255, opacity: 1.0))
+                .baselineOffset(1)
+                .font(.custom("Apple SD Gothic Neo", size: 20))
+            Text("4") // 데이터로 처리 필요
+                .font(.custom("Apple SD Gothic Neo", size: 20))
+                .foregroundColor(Color(red: 206 / 255, green: 206 / 255, blue: 206 / 255, opacity: 1.0))
+        }
+    }
+}
+
+struct DestinationCell: View {
+    var body: some View {
+        // if-else 문으로 나중에 들어오는 목적지 데이터에 따라서 포스텍<->포항역 배치 전환 해야함
+        HStack {
+            Spacer().frame(width: 10)
+            HStack {
+                Image("taxiOff")
+                    .resizable()
+                    .frame(width: 17, height: 15)
+                    .foregroundColor(Color(red: 64 / 255, green: 64 / 255, blue: 64 / 255, opacity: 1.0))
+                Text("포스텍")
+                    .fontWeight(.medium)
+                    .font(.custom("Apple SD Gothic Neo", size: 14))
+                    .foregroundColor(Color(red: 64 / 255, green: 64 / 255, blue: 64 / 255, opacity: 1.0))
+                Text("C5") // 나중에 데이터로 처리
+                    .fontWeight(.thin)
+                    .font(.custom("Apple SD Gothic Neo", size: 14))
+                    .foregroundColor(Color(red: 64 / 255, green: 64 / 255, blue: 64 / 255, opacity: 1.0))
+                    .padding(EdgeInsets(top: 1, leading: -5, bottom: 2, trailing: 1))
+            }.frame(minWidth: 90, minHeight: 20, maxHeight: 28, alignment: .center) // ex) 낙원교차로 등의 긴 문장이 와도 frame이 늘어남
+                .background(RoundedRectangle(cornerRadius: 8.0).fill(Color(red: 200 / 255, green: 1 / 255, blue: 80 / 255, opacity: 0.2)))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color(red: 200 / 255, green: 1 / 255, blue: 80 / 255, opacity: 1), lineWidth: 1.5)
+                )
+            Image("forward") // 나중에 이미지 update
+                .resizable()
+                .frame(width: 10, height: 15)
+                .padding(.horizontal, 7)
+            HStack {
+                Spacer().frame(width: 10)
+                Image(systemName: "tram.fill")
+                    .resizable()
+                    .frame(width: 13, height: 17)
+                    .foregroundColor(Color(red: 64 / 255, green: 64 / 255, blue: 64 / 255, opacity: 1.0))
+                Text("포항역")
+                    .fontWeight(.medium)
+                    .font(.custom("Apple SD Gothic Neo", size: 14))
+                    .foregroundColor(Color(red: 64 / 255, green: 64 / 255, blue: 64 / 255, opacity: 1.0))
+                Text("") // 나중에 데이터로 처리
+                    .fontWeight(.thin)
+                    .font(.custom("Apple SD Gothic Neo", size: 14))
+                    .foregroundColor(Color(red: 64 / 255, green: 64 / 255, blue: 64 / 255, opacity: 1.0))
+                    .padding(EdgeInsets(top: 1, leading: -5, bottom: 2, trailing: 1))
+                Spacer().frame(width: 3)
+            }.frame(minWidth: 40, minHeight: 20, maxHeight: 28, alignment: .center)
+                .background(RoundedRectangle(cornerRadius: 8.0).fill(Color(red: 24 / 255, green: 143 / 255, blue: 194 / 255, opacity: 0.2)))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color(red: 24 / 255, green: 143 / 255, blue: 194 / 255, opacity: 1), lineWidth: 1.5))
+        }
+    }
+}
+
+struct CellView_Previews: PreviewProvider {
+    static var previews: some View {
+        CellView()
+    }
+}
--- Taxi/Taxi/Presenter/TaxiPartyListView.swift
+++ Taxi/Taxi/Presenter/TaxiPartyListView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Yosep on 2022/06/09.
 //
-
 import SwiftUI
 
 struct TaxiPartyListView: View {
@@ -17,9 +16,27 @@ struct TaxiPartyListView: View {
                 Spacer()
                 MeetingDateChange()
             }
-            Spacer(minLength: 20)
-            CellViewList()
+            Spacer(minLength: 0)
+            ZStack {
+                ScrollView {
+                    CellViewList()
+                }
+                .frame(width: 400)
+                .background(Color(red: 248 / 255, green: 248 / 255, blue: 248 / 255, opacity: 1.0))
+                .overlay(
+                    Rectangle()
+                        .stroke(Color(red: 245 / 255, green: 245 / 255, blue: 245 / 255, opacity: 1.0), lineWidth: 1.5)
+                )
+                .ignoresSafeArea()
+                .refreshable {     // << injects environment value !!
+                    await fetchSomething()
+                }
+            }
         }.padding(20)
+    }
+    func fetchSomething() async {
+        // demo, assume we update something long here
+        try? await Task.sleep(nanoseconds: 3 * 1_000_000_000)
     }
 }
 
@@ -27,7 +44,7 @@ struct TaxiPartyHeadLine: View {
     var body: some View {
         HStack {
             Text("택시팟")
-                .font(.title)
+                .font(.custom("Apple SD Gothic Neo", size: 26))
                 .fontWeight(.bold)
             Spacer()
             Button {
@@ -35,7 +52,7 @@ struct TaxiPartyHeadLine: View {
             } label: {
                 Image(systemName: "plus")
                     .resizable()
-                    .frame(width: 25, height: 25)
+                    .frame(width: 26, height: 26)
                     .foregroundColor(.black)
             }
         }
@@ -43,41 +60,106 @@ struct TaxiPartyHeadLine: View {
 }
 
 struct TaxiPartyFiltering: View {
-    @State var destinations: [String] = ["전체", "포항역", "포스텍"]
+    let titles: [String] = ["전체", "포항역", "포스텍"]
+    @State var selectedIndex: Int = 0
+    var body: some View {
+        SegmentedPicker(
+            titles,
+            selectedIndex: Binding(
+                get: { selectedIndex },
+                set: { selectedIndex = $0 ?? 0 }),
+            content: { item, isSelected in
+                Text(item)
+                    .foregroundColor(Color.black)
+                    .font(.custom("Apple SD Gothic Neo", size: 16))
+                    .fontWeight(isSelected ? .semibold : .light)
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 8)
+            },
+            selection: {
+                VStack(spacing: 0) {
+                    Spacer()
+                    Rectangle()
+                        .fill(Color.black)
+                        .frame(width: 50, height: 2)
+                }
+            })
+        .onAppear {
+            selectedIndex = 0
+        }
+        .animation(.easeInOut(duration: 0.3), value: selectedIndex)
+    } // Switch 문으로 selectedIndex 별로 나누어 filter 되게끔 동작하게 한다.
+}
+
+struct MeetingDateChange: View {
+    var body: some View {
+        Button {
+            print("날짜 선택 tapped!")
+        } label: {
+            Text("날짜 선택")
+                .foregroundColor(Color(red: 255 / 255, green: 204 / 255, blue: 18 / 255, opacity: 1.0))
+                .font(.custom("Apple SD Gothic Neo", size: 16))
+                .fontWeight(.semibold)
+        }
+    }
+}
+
+struct MyProgress: View {
+    @State private var isProgress = false
     var body: some View {
         HStack {
-            ForEach(destinations, id: \.self) { name in
-                Button {
-                    print("filter tapped!")
-                } label: {
-                    Text(name)
-                        .padding(5)
-                        .foregroundColor(.black)
+            ForEach(0...4, id: \.self) { index in
+                Circle()
+                    .frame(width: 10, height: 10)
+                    .foregroundColor(Color(red: 255 / 255, green: 204 / 255, blue: 18 / 255, opacity: 1.0))
+                    .scaleEffect(self.isProgress ? 1:0.01)
+                    .animation(self.isProgress ? Animation .linear(duration: 0.8) .repeatForever() .delay(0.2*Double(index)) : .default, value: isProgress)
+            }
+        }
+        .onAppear { isProgress = true }
+        .padding()
+    }
+}
+
+struct CellViewList: View {
+    @Environment(\.refresh) private var refresh   // << refreshable injected !!
+    @State private var isRefreshing = false
+    var body: some View {
+        VStack {
+            if isRefreshing {
+                MyProgress()    // ProgressView() ?? - no, it's boring :)
+                    .transition(.scale)
+            }
+            ScrollView {
+                ForEach(0..<30, id: \.self) { _ in
+                    CellView()
+                        .padding(-4)
+                }
+            }
+        }
+        .animation(.default, value: isRefreshing)
+        .background(GeometryReader { // detect Pull-to-refresh
+            Color.clear.preference(key: ViewOffsetKey.self, value: -$0.frame(in: .global).origin.y)
+        })
+        .onPreferenceChange(ViewOffsetKey.self) {
+            if $0 < -300 && !isRefreshing {   // << any creteria we want !!
+                isRefreshing = true
+                Task {
+                    await refresh?()           // << call refreshable !!
+                    await MainActor.run {
+                        isRefreshing = false
+                    }
                 }
             }
         }
     }
 }
 
-struct MeetingDateChange: View {
-    var body: some View {
-        Button {
-            print("날짜변경 tapped!")
-        } label: {
-            Text("날짜 변경")
-                .foregroundColor(Color(red: 255 / 255, green: 214 / 255, blue: 0 / 255, opacity: 1.0))
-                .fontWeight(.bold)
-        }
-    }
-}
-
-struct CellViewList: View {
-    var body: some View {
-        ScrollView {
-            ForEach(0..<50) { num in
-                Text("Test: \(num)")
-            }
-        }
+struct ViewOffsetKey: PreferenceKey {
+    typealias Value = CGFloat
+    static var defaultValue = CGFloat.zero
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        value += nextValue()
     }
 }
 
