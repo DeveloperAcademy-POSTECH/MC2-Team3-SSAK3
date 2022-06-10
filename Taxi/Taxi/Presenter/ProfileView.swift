579a2307ee52b370653b07ad93010fff6c1c5b48
Chaeho-Min <james118@naver.com>
2022-06-10 15:58:05 +0900

    Refactor: Fix suggested changes
    코드 리뷰에서 받은 제안사항 반영

--- Taxi/Taxi/Presenter/ProfileView.swift
+++ Taxi/Taxi/Presenter/ProfileView.swift
@@ -8,31 +8,32 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @State private var isPresented: Bool = false
+    @State private var isPickerPresented: Bool = false
 
     var body: some View {
         VStack {
-            Image("ProfileDummy")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .clipShape(Circle())
-                .frame(width: 160)
-                .overlay(alignment: .bottom) {
-                    Text("편집")
-                }
-                .onTapGesture {
-                    isPresented.toggle()
-                }
-                .sheet(isPresented: $isPresented) {
-                    Text("PhotoPicker")
-                }
+            Button {
+                isPickerPresented.toggle()
+            } label: {
+                Image("ProfileDummy")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .frame(width: 160)
+                    .overlay(alignment: .bottom) {
+                        Text("편집")
+                    }
+            }
+            .sheet(isPresented: $isPickerPresented) {
+                Text("PhotoPicker")
+            }
             HStack {
                 Text("닉네임")
                 Spacer()
                 TextField("닉네임", text: .constant(""))
             }
             Button {
-                // Update User Data
+                // TODO: Update User Nickname, Profile Image
             } label: {
                 Text("적용")
             }
