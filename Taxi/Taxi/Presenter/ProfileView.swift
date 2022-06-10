a1c0c811f6d415e3241ff8d76a00a15de9cf1e7b
Chaeho-Min <james118@naver.com>
2022-06-10 20:01:39 +0900

    Feat: Add User Dummy Data in ProfileView

--- Taxi/Taxi/Presenter/ProfileView.swift
+++ Taxi/Taxi/Presenter/ProfileView.swift
@@ -9,6 +9,20 @@ import SwiftUI
 
 struct ProfileView: View {
     @State private var isPickerPresented: Bool = false
+    @State private var nickname: String
+    @State private var profileImage: String?
+
+    // Dummy Data
+    private var user: User = User(
+        id: "id1",
+        nickname: "Avo",
+        profileImage: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f202fce4-a5b6-4e40-9f5e-f22eaf4edd87/ProfileDummy.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220610%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220610T083701Z&X-Amz-Expires=86400&X-Amz-Signature=ec09e76610687de9121ac99b21ac9b186223d94641c8c4abf441d7f6ac8e268c&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22ProfileDummy.png%22&x-id=GetObject"
+    )
+
+    init() {
+        self.nickname = user.nickname
+        self.profileImage = user.profileImage
+    }
 
     var body: some View {
         VStack {
@@ -30,7 +44,7 @@ struct ProfileView: View {
             HStack {
                 Text("닉네임")
                 Spacer()
-                TextField("닉네임", text: .constant(""))
+                TextField("닉네임", text: $nickname)
             }
             Button {
                 // TODO: Update User Nickname, Profile Image
