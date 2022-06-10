b2f0c69387b1c90b2ae5f2fad701479329d7dbba
SH0123 <sanghyoboy@naver.com>
2022-06-10 21:04:02 +0900

    Feat: "mock data 추가 및 코드 구현"
    mock data를 추가하여 해당 날짜에 택시 팟 여부 체크할 수 있는지 함수 테스트

--- Taxi/Taxi/Domain/Model/TaxiParty.swift
+++ Taxi/Taxi/Domain/Model/TaxiParty.swift
@@ -33,3 +33,15 @@ extension TaxiParty {
         Place.of(destinationCode).rawValue
     }
 }
+
+func mockData() -> [TaxiParty] {
+    return [
+    TaxiParty(id: "1", departureCode: 0, destinationCode: 1, meetingDate: 20220621, meetingTime: 1420, maxPersonNumber: 4, members: ["아보", "제리", "RaymondMorning", "Woody"], isClosed: false),
+    TaxiParty(id: "2", departureCode: 0, destinationCode: 1, meetingDate: 20220610, meetingTime: 1331, maxPersonNumber: 3, members: ["아보", "레이몬드", "요셉"], isClosed: false),
+    TaxiParty(id: "3", departureCode: 1, destinationCode: 0, meetingDate: 20220608, meetingTime: 1831, maxPersonNumber: 4, members: ["호종", "종호", "효상", "상효"], isClosed: false),
+    TaxiParty(id: "4", departureCode: 1, destinationCode: 0, meetingDate: 20220630, meetingTime: 1931, maxPersonNumber: 4, members: ["인서", "서인", "윤영", "영윤"], isClosed: false),
+    TaxiParty(id: "5", departureCode: 0, destinationCode: 1, meetingDate: 20220709, meetingTime: 1431, maxPersonNumber: 3, members: ["채호", "호채", "승연"], isClosed: false),
+    TaxiParty(id: "6", departureCode: 1, destinationCode: 0, meetingDate: 20220621, meetingTime: 1931, maxPersonNumber: 3, members: ["연승", "효상", "인서"], isClosed: false)
+
+    ]
+}
