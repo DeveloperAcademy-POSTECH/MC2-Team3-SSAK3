d11b90d2a4980faadbdf88851df390070664d021
HoJong <pjh00098@gmail.com>
2022-06-10 15:55:29 +0900

    Add Taxi Party List Use case

---
+++ Taxi/Taxi/Domain/UseCase/JoinTaxiParty.swift
@@ -0,0 +1,20 @@
+//
+//  JoinTaxiParty.swift
+//  Taxi
+//
+//  Created by JongHo Park on 2022/06/10.
+//
+
+import Combine
+
+final class JoinTaxiPartyUseCase {
+    private let taxiPartyRepository: TaxiPartyRepository
+
+    init(_ taxiPartyRepository: TaxiPartyRepository = TaxiPartyFirebaseDataSource.shared) {
+        self.taxiPartyRepository = taxiPartyRepository
+    }
+
+    func joinTaxiParty(to taxiParty: TaxiParty, _ user: User) -> AnyPublisher<TaxiParty, Error> {
+        return taxiPartyRepository.joinTaxiParty(to: taxiParty)
+    }
+}
