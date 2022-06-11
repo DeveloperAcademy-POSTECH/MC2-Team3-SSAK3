11675e3da11d091387894811ef8c9e53639b0b39
Chaeho-Min <james118@naver.com>
2022-06-11 21:53:49 +0900

    Refactor: Fix suggested changes
    코드리뷰 사항 반영: 더미데이터 변경, 기타 가독성을 위해 코드 변경

--- Taxi/Taxi/Presenter/ProfileView.swift
+++ Taxi/Taxi/Presenter/ProfileView.swift
@@ -11,11 +11,22 @@ import SDWebImageSwiftUI
 struct ProfileView: View {
     @State private var showActionSheet: Bool = false
     @State private var showPicker: Bool = false
-    @State private var selectedImage: UIImage?
-    // DummyData
-    let id: String = "0"
-    @State private var nickname: String = "Avo"
-    @State private var urlFromData: String? = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f202fce4-a5b6-4e40-9f5e-f22eaf4edd87/ProfileDummy.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220611%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220611T030635Z&X-Amz-Expires=86400&X-Amz-Signature=8756de2e65dc2b6f616fb7a697bbebbaf8f779b53e3481e52fb183b4b5709821&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22ProfileDummy.png%22&x-id=GetObject"
+    @State private var selectedImage: UIImage? // 피커에서 선택한 사진을 담는 변수
+    @State private var nicknameContainer: String = "" // User 닉네임을 임시로 담는 변수
+    @State private var imageContainer: String? // User 프로필 사진 URL을 임시로 담는 변수
+    @State private var isProfileDeleted: Bool = false
+    private var profileSize: CGFloat = 160
+
+    @State private var user: User = User(
+        id: "1",
+        nickname: "Avo",
+        profileImage: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f202fce4-a5b6-4e40-9f5e-f22eaf4edd87/ProfileDummy.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220611%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220611T030635Z&X-Amz-Expires=86400&X-Amz-Signature=8756de2e65dc2b6f616fb7a697bbebbaf8f779b53e3481e52fb183b4b5709821&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22ProfileDummy.png%22&x-id=GetObject"
+    )
+
+    init() {
+        self._nicknameContainer = .init(initialValue: user.nickname)
+        self._imageContainer = .init(initialValue: user.profileImage)
+    }
 
     var body: some View {
         VStack {
@@ -25,14 +36,12 @@ struct ProfileView: View {
                 Group {
                     if let newImage = selectedImage { // 프로필 사진을 변경한 경우
                         Image(uiImage: newImage)
-                            .profileCircle(160)
-                    } else { // 프로필 사진을 변경하지 않은 경우
-                        if let imageURL = urlFromData { // 프로필 사진이 있는 경우
-                            WebImage(url: URL(string: imageURL))
-                                .profileCircle(160)
-                        } else { // 프로필 사진이 없는 경우
-                            textProfile(160)
-                        }
+                            .profileCircle(profileSize)
+                    } else if let imageURL = imageContainer { // 프로필 사진이 있는 경우
+                        WebImage(url: URL(string: imageURL))
+                            .profileCircle(profileSize)
+                    } else { // 프로필 사진이 없는 경우
+                        textProfile(profileSize)
                     }
                 }
                 .overlay(alignment: .bottom) {
@@ -48,11 +57,15 @@ struct ProfileView: View {
             HStack {
                 Text("닉네임")
                 Spacer()
-                TextField("닉네임", text: $nickname)
+                TextField("닉네임", text: $nicknameContainer)
                     .disableAutocorrection(true)
+                    .textInputAutocapitalization(.never)
             }
             Button {
                 // TODO: Update User Nickname, Profile Image
+                // if nicknameContainer != user.nickname { nickname = nicknameContainer }
+                // if let newImage = selectedImage { profileImage = Data(newImage) }
+                // else if isProfileDeleted { profileImage = nil }
             } label: {
                 Text("적용")
             }
@@ -68,7 +81,7 @@ extension ProfileView {
             Circle()
                 .foregroundColor(.gray)
                 .frame(width: diameter, height: diameter)
-            Text(nickname.prefix(1))
+            Text(nicknameContainer.prefix(1))
                 .foregroundColor(.black)
         }
     }
@@ -79,7 +92,8 @@ extension ProfileView {
             showPicker.toggle()
         }
         Button("프로필 사진 제거") {
-            urlFromData = nil
+            isProfileDeleted = true
+            imageContainer = nil
             selectedImage = nil
         }
         Button("취소", role: .cancel) {
