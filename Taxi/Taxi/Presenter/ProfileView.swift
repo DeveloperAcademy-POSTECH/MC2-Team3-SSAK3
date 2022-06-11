f39f5f4a8d945869934a2aa631b1581e03c7bef5
Chaeho-Min <james118@naver.com>
2022-06-11 12:10:38 +0900

    Feat: Add Photo from URL
    SDWebImageSwiftUI를 이용해 URL로부터 사진을 가져오는 기능 구현

--- Taxi/Taxi/Presenter/ProfileView.swift
+++ Taxi/Taxi/Presenter/ProfileView.swift
@@ -6,38 +6,43 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct ProfileView: View {
     @State private var showPicker: Bool = false
-    @State private var nickname: String
-    @State private var profileImage: String?
     @State private var newImage: UIImage?
 
-    // Dummy Data
-    private var user: User = User(
-        id: "id1",
-        nickname: "Avo",
-        profileImage: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f202fce4-a5b6-4e40-9f5e-f22eaf4edd87/ProfileDummy.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220610%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220610T083701Z&X-Amz-Expires=86400&X-Amz-Signature=ec09e76610687de9121ac99b21ac9b186223d94641c8c4abf441d7f6ac8e268c&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22ProfileDummy.png%22&x-id=GetObject"
-    )
-
-    init() {
-        self.nickname = user.nickname
-        self.profileImage = user.profileImage
-    }
+    // DummyData
+    let id: String = "0"
+    @State private var nickname: String = "Avo"
+    @State private var profileImage: String? = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f202fce4-a5b6-4e40-9f5e-f22eaf4edd87/ProfileDummy.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220611%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220611T030635Z&X-Amz-Expires=86400&X-Amz-Signature=8756de2e65dc2b6f616fb7a697bbebbaf8f779b53e3481e52fb183b4b5709821&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22ProfileDummy.png%22&x-id=GetObject"
 
     var body: some View {
         VStack {
             Button {
                 showPicker.toggle()
             } label: {
-                Image("ProfileDummy")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(Circle())
-                    .frame(width: 160)
+                if let imageURL = profileImage {
+                    WebImage(url: URL(string: imageURL))
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(Circle())
+                        .frame(width: 160)
+                        .overlay(alignment: .bottom) {
+                            Text("편집")
+                        }
+                } else {
+                    ZStack {
+                        Circle()
+                            .foregroundColor(.gray)
+                            .frame(width: 160, height: 160)
+                        Text(nickname.prefix(1))
+                            .foregroundColor(.black)
+                    }
                     .overlay(alignment: .bottom) {
                         Text("편집")
                     }
+                }
             }
             .sheet(isPresented: $showPicker) {
                 PhotoPicker(filter: .images, limit: 1) { results in
@@ -58,6 +63,7 @@ struct ProfileView: View {
                 Text("닉네임")
                 Spacer()
                 TextField("닉네임", text: $nickname)
+                    .disableAutocorrection(true)
             }
             Button {
                 // TODO: Update User Nickname, Profile Image
