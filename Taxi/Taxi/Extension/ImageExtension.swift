54df6d9127321dda2b658c261c197077b5b94996
Chaeho-Min <james118@naver.com>
2022-06-11 15:59:57 +0900

    Feat: Add ImageExtension
    Image나 WebImage로부터 원형의 프로필 뷰를 만들기 위한 메서드 추가

---
+++ Taxi/Taxi/Extension/ImageExtension.swift
@@ -0,0 +1,35 @@
+//
+//  ImageExtension.swift
+//  Taxi
+//
+//  Created by 민채호 on 2022/06/11.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+extension Image {
+    func profileCircle(_ diameter: CGFloat) -> some View {
+        self
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .clipShape(Circle())
+            .frame(width: diameter, height: diameter)
+    }
+}
+
+extension WebImage {
+    func profileCircle(_ diameter: CGFloat) -> some View {
+        self
+            .placeholder {
+                Circle().stroke().foregroundColor(.black)
+                    .overlay {
+                        ProgressView()
+                    }
+            }
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .clipShape(Circle())
+            .frame(width: diameter, height: diameter)
+    }
+}
