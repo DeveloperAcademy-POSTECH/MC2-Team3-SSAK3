ea0623fa556632b9b5537b8261488aa1c5648b34
Chaeho-Min <james118@naver.com>
2022-06-12 17:52:06 +0900

    Feat: Add PartyMemberInfo Component
    정보 확인 페이지에서 유저 아이디를 통해 택시팟 멤버 정보를 표현할 수 있는 컴포넌트 제작

--- Taxi/Taxi/Presenter/TaxiPartyInfoView.swift
+++ Taxi/Taxi/Presenter/TaxiPartyInfoView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct TaxiPartyInfoView: View {
     let taxiParty: TaxiParty
@@ -23,33 +24,9 @@ struct TaxiPartyInfoView: View {
             }
             Spacer()
             Group {
-                HStack {
-                    Image("ProfileDummy")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .clipShape(Circle())
-                        .frame(width: 80, height: 80)
-                    Text("Avo")
-                    Spacer()
-                }
-                HStack {
-                    Image("ProfileDummy")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .clipShape(Circle())
-                        .frame(width: 80, height: 80)
-                    Text("Avo")
-                    Spacer()
-                }
-                HStack {
-                    Image("ProfileDummy")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .clipShape(Circle())
-                        .frame(width: 80, height: 80)
-                    Text("Avo")
-                    Spacer()
-                }
+                PartyMemberInfo(id: taxiParty.members[0], diameter: 80)
+                PartyMemberInfo(id: taxiParty.members[0], diameter: 80)
+                PartyMemberInfo(id: taxiParty.members[0], diameter: 80)
                 HStack {
                     Circle()
                         .stroke(style: StrokeStyle(lineWidth: 1, dash: [10]))
@@ -79,6 +56,41 @@ struct TaxiPartyInfoView: View {
             RoundedButton("시작하기") {
                 // TODO: Add joinTaxiParty Action
             }
+        }
+    }
+}
+
+struct PartyMemberInfo: View {
+    let id: String
+    let diameter: CGFloat
+    private let user: User = User(
+        id: "id1",
+        nickname: "아보",
+        profileImage: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f202fce4-a5b6-4e40-9f5e-f22eaf4edd87/ProfileDummy.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220611%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220611T030635Z&X-Amz-Expires=86400&X-Amz-Signature=8756de2e65dc2b6f616fb7a697bbebbaf8f779b53e3481e52fb183b4b5709821&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22ProfileDummy.png%22&x-id=GetObject"
+    )
+
+    init(id: String, diameter: CGFloat) {
+        self.id = id
+        self.diameter = diameter
+        // TODO: id와 유즈케이스를 통해 유저 정보 가져오고 user 변수에 넣기
+    }
+
+    var body: some View {
+        HStack {
+            if let imageURL = user.profileImage {
+                WebImage(url: URL(string: imageURL))
+                    .profileCircle(diameter)
+            } else {
+                ZStack {
+                    Circle()
+                        .foregroundColor(.gray)
+                        .frame(width: diameter, height: diameter)
+                    Text(user.nickname.prefix(1))
+                        .foregroundColor(.black)
+                }
+            }
+            Text(user.nickname)
+            Spacer()
         }
     }
 }
