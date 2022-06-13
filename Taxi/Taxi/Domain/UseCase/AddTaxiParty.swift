ada5218c3a026e800ca3f29d3b11cc3435c961bc
HoJong <pjh00098@gmail.com>
2022-06-13 19:40:37 +0900

    Feat: Add AddTaxiParty Usecase API
    completion 클로저를 사용할 수 있는 API 추가

--- Taxi/Taxi/Domain/UseCase/AddTaxiParty.swift
+++ Taxi/Taxi/Domain/UseCase/AddTaxiParty.swift
@@ -8,6 +8,7 @@ import Combine
 
 final class AddTaxiPartyUseCase {
     private let taxiPartyRepository: TaxiPartyRepository
+    private var cancelBag: Set<AnyCancellable> = []
 
     init(taxiPartyRepository: TaxiPartyRepository = TaxiPartyFirebaseDataSource.shared) {
         self.taxiPartyRepository = taxiPartyRepository
@@ -15,5 +16,16 @@ final class AddTaxiPartyUseCase {
 
     func addTaxiParty(_ taxiParty: TaxiParty) -> AnyPublisher<TaxiParty, Error> {
         return taxiPartyRepository.addTaxiParty(taxiParty)
+    }
+
+    func addTaxiParty(_ taxiParty: TaxiParty, completion: @escaping (TaxiParty?, Error?) -> Void) {
+        taxiPartyRepository.addTaxiParty(taxiParty)
+            .sink { result in
+                if case let .failure(error) = result {
+                    completion(nil, error)
+                }
+            } receiveValue: { taxiParty in
+                completion(taxiParty, nil)
+            }.store(in: &cancelBag)
     }
 }
