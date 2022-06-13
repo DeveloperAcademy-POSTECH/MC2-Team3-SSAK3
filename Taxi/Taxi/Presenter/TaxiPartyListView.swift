77cf64c40e9874febb4bf5f09110002b0d55b930
Yosep <dlstj1212@naver.com>
2022-06-13 09:21:28 +0900

    Refactor: Add TaxiParty border, ignoresSafeArea()

--- Taxi/Taxi/Presenter/TaxiPartyListView.swift
+++ Taxi/Taxi/Presenter/TaxiPartyListView.swift
@@ -27,6 +27,7 @@ struct TaxiPartyListView: View {
                 }
             }.border(Color.gray)
                 .background(Color(red: 1 / 255, green: 248 / 255, blue: 248 / 255, opacity: 1.0))
+                .ignoresSafeArea()
         }
     }
     func fetchSomething() async {
