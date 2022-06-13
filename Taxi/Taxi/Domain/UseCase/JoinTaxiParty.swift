14f8ea39340962b5eb7cbe520e748186b5a1db43
HoJong <pjh00098@gmail.com>
2022-06-13 19:49:21 +0900

    Feat: Add JoinTaxiParty Usecase API

--- Taxi/Taxi/Domain/UseCase/JoinTaxiParty.swift
+++ Taxi/Taxi/Domain/UseCase/JoinTaxiParty.swift
@@ -9,6 +9,7 @@ import Combine
 
 final class JoinTaxiPartyUseCase {
     private let taxiPartyRepository: TaxiPartyRepository
+    private var cancelBag: Set<AnyCancellable> = []
 
     init(_ taxiPartyRepository: TaxiPartyRepository = TaxiPartyFirebaseDataSource.shared) {
         self.taxiPartyRepository = taxiPartyRepository
@@ -16,5 +17,16 @@ final class JoinTaxiPartyUseCase {
 
     func joinTaxiParty(in taxiParty: TaxiParty, _ user: User) -> AnyPublisher<TaxiParty, Error> {
         return taxiPartyRepository.joinTaxiParty(to: taxiParty)
+    }
+
+    func joinTaxiParty(in taxiParty: TaxiParty, _ user: User, completion: @escaping (TaxiParty?, Error?) -> Void) {
+        taxiPartyRepository.joinTaxiParty(to: taxiParty)
+            .sink { result in
+                if case let .failure(error) = result {
+                    completion(nil, error)
+                }
+            } receiveValue: { taxiParty in
+                completion(taxiParty, nil)
+            }.store(in: &cancelBag)
     }
 }
