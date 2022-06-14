2be7665b45071c34521678952ccce22d86371a9a
HoJong <pjh00098@gmail.com>
2022-06-14 14:51:54 +0900

    Feat: Add AddTaxiParty Info Step

--- Taxi/Taxi/Presenter/AddTaxiParty/AddTaxiParty.swift
+++ Taxi/Taxi/Presenter/AddTaxiParty/AddTaxiParty.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 
+// MARK: - 정보를 입력하는 각 단계를 지칭하는 enumeration
+extension AddTaxiParty {
+    enum Step {
+        case none
+        case destination
+        case date
+        case time
+        case departure
+        case personNumber
+    }
+}
 struct AddTaxiParty: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
