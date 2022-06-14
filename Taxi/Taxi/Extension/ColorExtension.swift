c255ca0dbf005f9e3c9f38542fd94e9f8846b5eb
SH0123 <sanghyoboy@naver.com>
2022-06-14 12:20:53 +0900

    Feat: Color & text style
    색상 추가 및 글자 스타일 추가

--- Taxi/Taxi/Extension/ColorExtension.swift
+++ Taxi/Taxi/Extension/ColorExtension.swift
@@ -7,6 +7,7 @@
 import SwiftUI
 
 extension Color {
+    static let customGreen = Color("customGreen")
     static let customYellow = Color("customYellow")
     static let customBlack = Color("customBlack")
     static let charcoal = Color("charcoal")
@@ -18,5 +19,6 @@ extension Color {
     static let postechPink = Color("postechPink")
     static let ktxBlue = Color("ktxBlue")
     static let selectYellow = Color("selectYellow")
+    static let signUpYellowGray  = Color("signUpYellowGray")
     static let addBackground = Color("addBackground")
 }
