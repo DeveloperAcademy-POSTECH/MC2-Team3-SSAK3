7e86be4528c6d4d18d514f8ceff1c23501e7d9ae
Chaeho-Min <james118@naver.com>
2022-06-15 21:48:51 +0900

    Refactor: Change MyPageView Code

--- Taxi/Taxi/Presenter/MyPageView.swift
+++ Taxi/Taxi/Presenter/MyPageView.swift
@@ -18,32 +18,12 @@ struct MyPageView: View {
     var body: some View {
         VStack(alignment: .leading) {
             Text("설정")
-            HStack {
-                if let user = userViewModel.user {
-                    ProfileImage(user, diameter: 46)
-                    Text(user.nickname)
-                } else {
-                    Circle().foregroundColor(.lightGray).frame(width: 46, height: 46)
-                    Rectangle().foregroundColor(.lightGray).frame(width: 60, height: 20)
-                }
-                Spacer()
-                Button {
-                    showProfile.toggle()
-                } label: {
-                    Text("프로필 수정")
-                }
-            }
+            profile
             Divider()
             Text("알림")
             notificationSetting("채팅 알림", isOn: $chattingNoti)
             notificationSetting("택시팟 완료 알림", isOn: $partyCompleteNoti)
             Divider()
-            logOnStateButton("로그아웃", label: "정말 로그아웃 하시겠습니까?", message: "경고문구", isPresented: $isTryLogout) {
-                // TODO: Add Logout Action
-            }
-            logOnStateButton("회원탈퇴", label: "정말 회원탈퇴 하시겠습니까?", message: "경고문구", isPresented: $isTryWithdrawal) {
-                // TODO: Add Withdrawal Action
-            }
             Spacer(minLength: 0)
         }
         .sheet(isPresented: $showProfile) {
@@ -52,30 +32,32 @@ struct MyPageView: View {
     }
 }
 
-extension MyPageView {
+private extension MyPageView {
 
-    private func notificationSetting(_ label: String, isOn: Binding<Bool>) -> some View {
+    var profile: some View {
+        HStack {
+            if let user = userViewModel.user {
+                ProfileImage(user, diameter: 46)
+                Text(user.nickname)
+            } else {
+                Circle().foregroundColor(.lightGray).frame(width: 46, height: 46)
+                Rectangle().foregroundColor(.lightGray).frame(width: 60, height: 20)
+            }
+            Spacer()
+            Button {
+                showProfile.toggle()
+            } label: {
+                Text("프로필 수정")
+            }
+        }
+    }
+
+    func notificationSetting(_ label: String, isOn: Binding<Bool>) -> some View {
         HStack {
             Text(label)
             Spacer()
             Toggle(label, isOn: isOn)
                 .labelsHidden()
-        }
-    }
-
-    private func logOnStateButton(_ title: String, label: String, message: String, isPresented: Binding<Bool>, action: @escaping () -> Void) -> some View {
-        Button {
-            isPresented.wrappedValue.toggle()
-        } label: {
-            Text(title)
-        }
-        .alert(label, isPresented: isPresented) {
-            Button("취소", role: .cancel) {
-                isPresented.wrappedValue.toggle()
-            }
-            Button("확인", role: .destructive, action: action)
-        } message: {
-            Text(message)
         }
     }
 }
