9941e347e144a74e42dd9d93ac132436250ef7f7
SH0123 <sanghyoboy@naver.com>
2022-06-16 11:24:13 +0900

    Feat: calendar modal 로직 구현

--- Taxi/Taxi/Presenter/CalendarModal.swift
+++ Taxi/Taxi/Presenter/CalendarModal.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CalendarModal: View {
     @Binding var isShowing: Bool
+    @State private var renderedDate: Date?
+    @State private var toastIsShowing = false
     @State private var isPresented = false
     @State private var curHeight: CGFloat = 400
     private let minHeight: CGFloat = 300
@@ -27,8 +29,14 @@ struct CalendarModal: View {
                             isShowing = false
                         }
                     }
-                mainView
-                    .transition(.move(edge: .bottom))
+                VStack {
+                    if toastIsShowing {
+                        toastMessage
+                    }
+                    mainView
+                    //.transition(.move(edge: .bottom))
+                    // TODO: 애니메이션 일단 taxipartylist에서 제거 후 달력 개선
+                }
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
@@ -37,27 +45,71 @@ struct CalendarModal: View {
 
     var mainView: some View {
         VStack {
-            ZStack {
-                Capsule()
-                    .frame(width: 40, height: 6)
+            sheetHeader
+                .padding([.bottom, .top], 15)
+            CalendarView(taxiParties: TaxiPartyMockData.mockData) {isTaxiParty, selectedDate in
+                if isTaxiParty {
+                    withAnimation {
+                        toastIsShowing = false
+                    }
+                    renderedDate = selectedDate
+                } else {
+                    withAnimation {
+                        toastIsShowing = true
+                    }
+                }
             }
-            .frame(height: 40)
-            .frame(maxWidth: .infinity)
-            .background(Color.white.opacity(0.00001))
-            NavigationView {
-            } // TODO: 차후 달력 컴포넌트와 연동 예정
-            .frame(maxHeight: .infinity)
-            .padding(.bottom, 35)
         }
-        .frame(height: curHeight)
+        .padding()
         .frame(maxWidth: .infinity)
         .background(
             ZStack {
-                RoundedRectangle(cornerRadius: 10)
                 Rectangle()
-                    .frame(height: curHeight / 2)
+                    .cornerRadius(10)
             }
                 .foregroundColor(.white)
         )
+    }
+
+    var sheetHeader: some View {
+        HStack {
+            Button {
+                withAnimation {
+                    isShowing.toggle()
+                }
+            } label: {
+                Text("닫기")
+            }
+            Spacer()
+            Text("날짜를 선택해주세요")
+            Spacer()
+            Button {
+                // TODO: scrollview reader scroll to renderedDate
+                withAnimation {
+                    isShowing.toggle()
+                }
+
+            } label: {
+                Text("확인")
+            }
+        }
+    }
+    
+    var toastMessage: some View {
+        Text("해당 날짜에 생성된 택시팟이 없어요")
+            .padding(EdgeInsets(top: 15, leading: 40, bottom: 15, trailing: 40))
+
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.white)
+            )
+                .frame(maxWidth: .infinity)
+            .padding(7)
+    }
+}
+
+struct CalendarModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaxiPartyListView()
     }
 }
