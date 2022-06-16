161b63ce63b585dcf39aabdee663c833ddac6744
Yosep <dlstj1212@naver.com>
2022-06-16 15:45:29 +0900

    Feat: Add Calender Date Filtering

--- Taxi/Taxi/Presenter/CalendarModal.swift
+++ Taxi/Taxi/Presenter/CalendarModal.swift
@@ -8,15 +8,13 @@
 import SwiftUI
 
 struct CalendarModal: View {
+    @ObservedObject var taxiPartyListViewModel: TaxiPartyListViewModel
     @Binding var isShowing: Bool
-    @State private var renderedDate: Date?
+    @Binding var renderedDate: Date?
+    @State private var storeDate: Date?
     @State private var toastIsShowing = false
     @State private var isPresented = false
     @State private var curHeight: CGFloat = 400
-    private let minHeight: CGFloat = 300
-    private let maxHeight: CGFloat = 300
-    private let startOpacity: Double = 0.4
-    private let endOpacity: Double = 0.8
 
     var body: some View {
         ZStack(alignment: .bottom) {
@@ -34,8 +32,6 @@ struct CalendarModal: View {
                         toastMessage
                     }
                     mainView
-                    //.transition(.move(edge: .bottom))
-                    // TODO: 애니메이션 일단 taxipartylist에서 제거 후 달력 개선
                 }
             }
         }
@@ -47,14 +43,14 @@ struct CalendarModal: View {
         VStack {
             sheetHeader
                 .padding([.bottom, .top], 15)
-            CalendarView(taxiParties: TaxiPartyMockData.mockData) {isTaxiParty, selectedDate in
+            CalendarView(taxiParties: taxiPartyListViewModel.taxiPartyList) {isTaxiParty, selectedDate in
                 if isTaxiParty {
                     withAnimation {
                         toastIsShowing = false
                     }
-                    renderedDate = selectedDate
+                    storeDate = selectedDate
                 } else {
-                    renderedDate = nil
+                    storeDate = nil
                     withAnimation {
                         toastIsShowing = true
                     }
@@ -85,28 +81,26 @@ struct CalendarModal: View {
             Text("날짜를 선택해주세요")
             Spacer()
             Button {
-                // TODO: scrollview reader scroll to renderedDate
                 withAnimation {
                     isShowing.toggle()
                 }
-
+                renderedDate = storeDate
             } label: {
                 Text("확인")
             }
-            .disabled(renderedDate == nil)
+            .disabled(storeDate == nil)
         }
     }
-    
+
     var toastMessage: some View {
         Text("해당 날짜에 생성된 택시팟이 없어요")
-            .padding(EdgeInsets(top: 15, leading: 40, bottom: 15, trailing: 40))
-
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 15)
             .background(
                 RoundedRectangle(cornerRadius: 10)
                     .fill(.white)
             )
-                .frame(maxWidth: .infinity)
-            .padding(7)
+            .padding()
     }
 }
 
