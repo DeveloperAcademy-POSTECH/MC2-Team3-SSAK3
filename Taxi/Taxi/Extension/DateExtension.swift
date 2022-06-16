809a668058aa8b1ff77777134171f9bd586c140b
SH0123 <sanghyoboy@naver.com>
2022-06-16 16:34:16 +0900

    Feat: date, day formatter 생성

--- Taxi/Taxi/Extension/DateExtension.swift
+++ Taxi/Taxi/Extension/DateExtension.swift
@@ -64,6 +64,30 @@ extension Date {
         return self.dateComponents.minute
     }
 
+    var weekday: String {
+            let weekIndex = self.dateComponents.weekday
+            guard let weekIndex = weekIndex else { return "" }
+
+            switch weekIndex {
+            case 1:
+                return "일요일"
+            case 2:
+                return "월요일"
+            case 3:
+                return "화요일"
+            case 4:
+                return "수요일"
+            case 5:
+                return "목요일"
+            case 6:
+                return "금요일"
+            case 7:
+                return "토요일"
+            default:
+                return ""
+            }
+        }
+
     var formattedString: String {
         return Date.formatter.string(from: self)
     }
@@ -137,7 +161,7 @@ extension Date {
 
     static func convertToKoreanDateFormat(from date: Int) -> String {
         guard let date = self.convertToDateFormat(from: date), let month = date.month, let day = date.day else { return "" }
-        return "\(month)월 \(day)일"
+        return "\(month)월 \(day)일 \(date.weekday)"
     }
 
     static func convertMessageTimeToReadable(from timeStamp: Int) -> String {
