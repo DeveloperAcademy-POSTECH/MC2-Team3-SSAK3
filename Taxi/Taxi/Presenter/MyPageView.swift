a3e3e120601258840427c71a2fa3a5b8a4b7be55
Chaeho-Min <james118@naver.com>
2022-06-16 12:21:50 +0900

    Feat: Add Hifi in MyPageView

--- Taxi/Taxi/Presenter/MyPageView.swift
+++ Taxi/Taxi/Presenter/MyPageView.swift
@@ -16,14 +16,20 @@ struct MyPageView: View {
     @EnvironmentObject var userViewModel: Authentication
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("설정")
+        VStack(alignment: .leading, spacing: 16) {
+            Rectangle()
+                .foregroundColor(.white)
+                .frame(height: 60)
+                .overlay(alignment: .bottomLeading) {
+                    Text("설정")
+                        .mainTitle()
+                        .padding(.leading)
+                }
             profile
-            Divider()
-            Text("알림")
+            Rectangle().fill(Color(red: 240 / 255, green: 240 / 255, blue: 240 / 255)).frame(height: 5)
+            notificationHeader
             notificationSetting("채팅 알림", isOn: $chattingNoti)
             notificationSetting("택시팟 완료 알림", isOn: $partyCompleteNoti)
-            Divider()
             Spacer(minLength: 0)
         }
         .sheet(isPresented: $showProfile) {
@@ -35,10 +41,12 @@ struct MyPageView: View {
 private extension MyPageView {
 
     var profile: some View {
-        HStack {
+        HStack(spacing: 13) {
             if let user = userViewModel.user {
                 ProfileImage(user, diameter: 46)
                 Text(user.nickname)
+                    .foregroundColor(.charcoal)
+                    .font(Font.custom("AppleSDGothicNeo-Medium", size: 18))
             } else {
                 Circle().foregroundColor(.lightGray).frame(width: 46, height: 46)
                 Rectangle().foregroundColor(.lightGray).frame(width: 60, height: 20)
@@ -48,17 +56,32 @@ private extension MyPageView {
                 showProfile.toggle()
             } label: {
                 Text("프로필 수정")
+                    .font(Font.custom("AppleSDGothicNeo-Medium", size: 12))
+                    .padding(EdgeInsets(top: 8, leading: 4, bottom: 8, trailing: 4))
+                    .background(RoundedRectangle(cornerRadius: 5).stroke().fill(Color.customGray))
             }
         }
+        .padding(.horizontal)
+    }
+
+    var notificationHeader: some View {
+        Text("알림")
+            .font(Font.custom("AppleSDGothicNeo-Medium", size: 18))
+            .foregroundColor(.darkGray)
+            .padding(.leading)
     }
 
     func notificationSetting(_ label: String, isOn: Binding<Bool>) -> some View {
         HStack {
             Text(label)
+                .font(Font.custom("AppleSDGothicNeo-Medium", size: 18))
+                .foregroundColor(.charcoal)
             Spacer()
             Toggle(label, isOn: isOn)
                 .labelsHidden()
+                .tint(.customYellow)
         }
+        .padding(.horizontal)
     }
 }
 
