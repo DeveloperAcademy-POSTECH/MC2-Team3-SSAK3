b70f0c9ad66accffedac08dc678f2c9d9b339768
HoJong <pjh00098@gmail.com>
2022-07-09 09:54:13 +0900

    Feat: Add Reigster Use Case

--- Taxi/Taxi/Presenter/ViewModel/Authentication.swift
+++ Taxi/Taxi/Presenter/ViewModel/Authentication.swift
@@ -12,17 +12,37 @@ final class Authentication: ObservableObject {
     @Published private (set) var userInfo: UserInfo?
     private let authenticateUseCase: AuthenticateUseCase
     private let loginUseCase: LoginUseCase
+    private let registerUseCase: RegisterUseCase
     private var cancelBag: Set<AnyCancellable> = []
 
     init(authenticateUseCase: AuthenticateUseCase = AuthenticateUseCase(),
-         loginUseCase: LoginUseCase = LoginUseCase()) {
+         loginUseCase: LoginUseCase = LoginUseCase(),
+         registerUseCase: RegisterUseCase = RegisterUseCase()) {
         self.authenticateUseCase = authenticateUseCase
         self.loginUseCase = loginUseCase
+        self.registerUseCase = registerUseCase
         login(with: "")
     }
 
     func login(with email: Email) {
         loginUseCase.login(with: email)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print(error)
+                case .finished:
+                    print("finished")
+                }
+            } receiveValue: { [weak self] userInfo in
+                guard let self = self else {
+                    return
+                }
+                self.userInfo = userInfo
+            }.store(in: &cancelBag)
+    }
+
+    func register(_ id: String, nickname: String) {
+        registerUseCase.register(id, nickname: nickname)
             .sink { completion in
                 switch completion {
                 case .failure(let error):
