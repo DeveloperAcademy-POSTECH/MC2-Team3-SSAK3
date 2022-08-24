0e56b7a6ab00867981bf6754b03d7cca60a06948
Chaeho-Min <james118@naver.com>
2022-08-24 19:23:05 +0900

    Bug: Fix InfoView dismiss bug

--- Taxi/Taxi/Presenter/TaxiPartyList/TaxiPartyList.swift
+++ Taxi/Taxi/Presenter/TaxiPartyList/TaxiPartyList.swift
@@ -134,7 +134,12 @@ private extension TaxiPartyList {
                 .fullScreenCover(isPresented: $isShowTaxiPartyInfo) {
                     showBlur = false
                 } content: {
-                    TaxiPartyInfo(taxiParty: taxiParty, viewModel: viewModel, showBlur: $showBlur)
+                    TaxiPartyInfo(
+                        taxiParty: taxiParty,
+                        viewModel: viewModel,
+                        showBlur: $showBlur,
+                        isShowTaxiPartyInfo: $isShowTaxiPartyInfo
+                    )
                 }
                 .contentShape(RoundedRectangle(cornerRadius: 16))
                 .listRowSeparator(.hidden)
