68ef7f19d715b58e106aba5c4c25926dd2c7188d
Yosep <dlstj1212@naver.com>
2022-09-21 03:11:48 +0900

    Feat: Change wording "cafebene to toilet"

--- Taxi/TaxiTests/AddTaxiPartyUsecaseTest.swift
+++ Taxi/TaxiTests/AddTaxiPartyUsecaseTest.swift
@@ -13,7 +13,7 @@ class AddTaxiPartyUsecaseTest: XCTestCase {
 
     func testAddTaxiPartyCompletionHandler() {
         // given
-        let taxiParty: TaxiParty = TaxiParty(id: "캐쉰캐샤", departureCode: Place.cafebene.toCode(), destinationCode: Place.pohangStation.toCode(), meetingDate: 1, meetingTime: 1, maxPersonNumber: 3, members: ["1"], isClosed: false)
+        let taxiParty: TaxiParty = TaxiParty(id: "캐쉰캐샤", departureCode: Place.toilet.toCode(), destinationCode: Place.pohangStation.toCode(), meetingDate: 1, meetingTime: 1, maxPersonNumber: 3, members: ["1"], isClosed: false)
         let promise = expectation(description: "Add Taxi Party Success!")
         let user: UserInfo = UserInfo(id: "테스트 유저", nickname: "", profileImage: "")
         var error: Error?
--- Taxi/TaxiTests/JoinTaxiPartyTest.swift
+++ Taxi/TaxiTests/JoinTaxiPartyTest.swift
@@ -14,7 +14,7 @@ class JoinTaxiPartyTest: XCTestCase {
     func testJoinTaxiPartyCompletionHandler() {
         // given
         let promise = expectation(description: "Join TaxiParty Success!")
-        let taxiParty: TaxiParty = TaxiParty(id: "캐쉰캐샤", departureCode: Place.cafebene.toCode(), destinationCode: Place.pohangStation.toCode(), meetingDate: 1, meetingTime: 1, maxPersonNumber: 3, members: ["1"], isClosed: false)
+        let taxiParty: TaxiParty = TaxiParty(id: "캐쉰캐샤", departureCode: Place.toilet.toCode(), destinationCode: Place.pohangStation.toCode(), meetingDate: 1, meetingTime: 1, maxPersonNumber: 3, members: ["1"], isClosed: false)
         let user: UserInfo = UserInfo(id: "1125125125", nickname: "호종이", profileImage: nil)
         var error: Error?
         // when
